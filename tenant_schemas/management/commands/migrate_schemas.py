4493ce6a6585f241a3baa3bb58f758936a3c133a
Jeryn Mathew <jerynmathew@gmail.com>
2020-02-14 00:43:20 +0530

    Fixed type issue with migrate-schemas command

--- tenant_schemas/management/commands/migrate_schemas.py
+++ tenant_schemas/management/commands/migrate_schemas.py
@@ -46,6 +46,6 @@ class Command(SyncCommon):
                 else:
                     tenants = [self.schema_name]
             else:
-                tenants = get_tenant_model().objects.using(db).exclude(schema_name=get_public_schema_name()).values_list(
-                    'schema_name', flat=True)
+                tenants = list(get_tenant_model().objects.using(db).exclude(schema_name=get_public_schema_name())
+                                                                   .values_list('schema_name', flat=True))
             executor.run_migrations(tenants=tenants)
